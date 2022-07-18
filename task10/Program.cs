0fb4d0d197f5b35b7405f2af370e7f8a36969955
Esmghan <11stv.ru@mail.ru>
2022-07-18 13:07:23 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit = number/10 % 10;
+Console.WriteLine ($"Вторая цифра трехзначного числа = {digit}");
+
