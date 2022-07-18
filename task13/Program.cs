c5b7b8f4089d0b335d5b9aeb05866cfa2c6c137a
Esmghan <11stv.ru@mail.ru>
2022-07-18 20:20:45 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+if(number<100) 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else{
+    string figure=number.ToString();
+    Console.WriteLine(figure[2]);
+}
